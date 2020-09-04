0b81afba8c826aa1f6dacb02d8e056458e433f88
DylanJansen <40144787+DylanJansen@users.noreply.github.com>
2020-09-04 07:44:33 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,50 @@
+var Discord = require('discord.js');
+var bot = new Discord.Client();
+var fs = require('fs');
+var ms = require("ms");
+bot.commands = new Discord.Collection();
+
+
+fs.readdir('./commands/', (err, files) => {
+  if(err) console.error(err);
+
+  var jsfiles = files.filter(f => f.split('.').pop() === 'js');
+  if (jsfiles.length <= 0){ return console.log('Geen commands gevonden...')}
+  else { console.log(jsfiles.length + ' commands gevonden.') }
+
+  jsfiles.forEach((f, i) => {
+    var cmds = require(`./commands/${f}`);
+    console.log(`command ${f} loading...`);
+    bot.commands.set(cmds.config.command, cmds);
+  });
+});
+
+bot.on('message', message => {
+
+    if (message.author.bot) return;
+    const prefix = '!';
+
+
+    if (!message.content.startsWith(prefix)) return; // return if no prefix in message start
+
+
+    const args = message.content.slice(prefix.length).split(' ');
+    const command = args.shift().toLowerCase();
+
+
+
+    const cmd = bot.commands.get(command);
+    if (cmd) cmd.run(bot, message, args);
+
+
+
+
+});
+
+bot.on("ready", async () => {
+  console.log(`${bot.user.username} is online!`);
+});
+
+
+
+bot.login('NDQ3Njc2NzY1MDg3NzI3NjE3.WwExeg.AB13RLaGmZplUOmsInWT78hZErI')
